b69e5e66abd31a96200bb9892535410cbb9c9589
isshort <namatullahwahidi@gmail.com>
2022-05-16 15:24:48 +0600

    tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_learn/101/list_view_builder.dart';
 import 'package:flutter_learn/101/navigation_learn.dart';
+import 'package:flutter_learn/202/tabbar_learn.dart';
 
 import 'package:flutter_learn/demos/color_life_cycle_view.dart';
 
@@ -19,6 +20,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.dark().copyWith(
+          tabBarTheme: TabBarTheme(
+            labelColor: Colors.red,
+            indicatorSize: TabBarIndicatorSize.label,
+            unselectedLabelColor: Colors.black,
+          ),
+          bottomAppBarTheme: BottomAppBarTheme(
+            shape: CircularNotchedRectangle(),
+          ),
           progressIndicatorTheme: const ProgressIndicatorThemeData(color: Colors.white),
           appBarTheme: const AppBarTheme(
             centerTitle: true,
@@ -27,7 +36,7 @@ class MyApp extends StatelessWidget {
             elevation: 0,
             // backgroundColor: Colors.red,
           )),
-      home: const NavigationLearn(),
+      home: const TabBarLearn(),
     );
   }
 }
